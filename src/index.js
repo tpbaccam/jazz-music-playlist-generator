8ab82b2fd7f81abdaebfca13fb78b05a1ff380bb
tpbaccam <tpbaccam@gmail.com>
2026-09-06 12:08:45 -0700

    Remove js on playlist generating update

--- src/index.js
+++ src/index.js
@@ -8,15 +8,13 @@ function displayPlaylist(response) {
 }
 function generatePlaylist(event) {
   event.preventDefault();
+
   let userInput = document.querySelector("#userInput");
   let apiKey = "f06ffeb6b437a2d43f8900ca0tf29cod";
   let prompt =
     "You are a modern jazz music expert. Please generate a playlist of 5 modern jazz songs based on the user's input.";
   let context = `Generate a modern jazz playlist based on the following input: ${userInput.value}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag before the first song title and after each song title and artist. Each song title should be <strong> `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  let playlistContainer = document.querySelector("#playlistContainer");
-  playlistContainer.innerHTML = `<div class = "generating"> ⏳ Generating a jazz playlist inspired by ${userInput.value}</div>`;
 
   console.log("Generating playlist...");
   console.log(`Prompt: ${prompt}`);
