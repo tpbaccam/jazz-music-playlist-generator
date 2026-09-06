4bcc9fe210eb4ba849ee4dc32e10679536fed728
tpbaccam <tpbaccam@gmail.com>
2026-09-06 11:05:28 -0700

    Adding Axios in JS and function to generate display playlist

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
+function displayPlaylist(response) {
+  new Typewriter("#playlistResults", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 function generatePlaylist(event) {
   event.preventDefault();
 
   let apiKey = "f06ffeb6b437a2d43f8900ca0tf29cod";
   let prompt =
     "You are a jazz music expert. Please generate a playlist of 5 jazz songs based on the user's input.";
-  let context = `Generate a jazz playlist based on the following input: ${prompt}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists.`;
+  let context = `Generate a jazz playlist based on the following input: ${prompt}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag after each song title and artist. `;
   let apiUrl =
     "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
 
-  new Typewriter("#playlistContainer", {
-    strings:
-      "Miles Davis, John Coltrane, Ella Fitzgerald, Duke Ellington, Billie Holiday, Thelonious Monk, Charlie Parker, Louis Armstrong, Chet Baker, Herbie Hancock",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  console.log("generating jazz playlist...");
+
   axios.get(apiUrl).then(displayPlaylist);
 }
 
