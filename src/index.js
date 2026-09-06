d69b0a8ba784cf85397876c52889f1de1407068d
tpbaccam <tpbaccam@gmail.com>
2026-09-06 12:01:56 -0700

    Update playlist appearance and UX

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ function generatePlaylist(event) {
   let context = `Generate a modern jazz playlist based on the following input: ${userInput.value}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag before the first song title and after each song title and artist. Each song title should be <strong> `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let playlistContainer = document.querySelector("#playlistResults");
-  playlistContainer.style.display = "block";
+  let playlistContainer = document.querySelector("#playlistContainer");
+  playlistContainer.classList.remove("hidden");
+  playlistContainer.innerHTML = `<div class = "generating"> ⏳ Generating a jazz playlist inspired by ${userInput.value}</div>`;
 
   console.log("Generating playlist...");
   console.log(`Prompt: ${prompt}`);
