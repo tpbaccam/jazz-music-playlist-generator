e5729dac0939e632e5b694dd7d3cc6fb2ff6ba39
tpbaccam <tpbaccam@gmail.com>
2026-09-06 11:25:28 -0700

    Updating user input in JS

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ function displayPlaylist(response) {
 }
 function generatePlaylist(event) {
   event.preventDefault();
-
+  let userInput = document.querySelector("#userInput");
   let apiKey = "f06ffeb6b437a2d43f8900ca0tf29cod";
   let prompt =
-    "You are a modernjazz music expert. Please generate a playlist of 5 modern jazz songs based on the user's input.";
-  let context = `Generate a modern jazz playlist based on the following input: ${prompt}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag after each song title and artist. `;
+    "You are a modern jazz music expert. Please generate a playlist of 5 modern jazz songs based on the user's input.";
+  let context = `Generate a modern jazz playlist based on the following input: ${userInput.value}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag after each song title and artist. `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating jazz playlist...");
+  console.log("Generating playlist...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
 
   axios.get(apiUrl).then(displayPlaylist);
 }
