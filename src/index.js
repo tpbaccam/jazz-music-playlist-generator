539eaf53c304bc0c13809444feec6817411791d7
tpbaccam <tpbaccam@gmail.com>
2026-09-06 11:12:51 -0700

    update to modern jazz in context and prompt

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ function generatePlaylist(event) {
 
   let apiKey = "f06ffeb6b437a2d43f8900ca0tf29cod";
   let prompt =
-    "You are a jazz music expert. Please generate a playlist of 5 jazz songs based on the user's input.";
-  let context = `Generate a jazz playlist based on the following input: ${prompt}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag after each song title and artist. `;
+    "You are a modernjazz music expert. Please generate a playlist of 5 modern jazz songs based on the user's input.";
+  let context = `Generate a modern jazz playlist based on the following input: ${prompt}. Please provide a list of 5 song titles that fit the jazz theme with their respective artists. In the list please include a <br /> tag after each song title and artist. `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating jazz playlist...");
